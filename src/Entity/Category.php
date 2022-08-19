0f1d67d8a086070c7b00ee7feda18e03833f1a67
shahryar.jahanshahloo <shahryar.jahanshahloo@gmail.com>
2022-08-19 20:14:00 +0430

    added category entity

---
+++ src/Entity/Category.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    #[ORM\OneToOne(targetEntity: "Category")]
+    #[ORM\JoinColumn(name: "parent_id", referencedColumnName: "id")]
+    private ?int $parent = null;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getParent(): ?string
+    {
+        return $this->parent;
+    }
+
+    public function setParent(string $parent): self
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+}
