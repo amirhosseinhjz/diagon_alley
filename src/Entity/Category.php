a359d0219e4cd05440c6ce860c7b85b8414ac0ab
shahryar.jahanshahloo <shahryar.jahanshahloo@gmail.com>
2022-08-19 20:14:00 +0430

    added category entity

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -4,10 +4,6 @@ namespace App\Entity;
 
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Symfony\Component\Serializer\Annotation\Groups;
-use Symfony\Component\Serializer\Annotation\MaxDepth;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 class Category
@@ -15,44 +11,16 @@ class Category
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(['category_basic'])]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255, unique: true, nullable: false)]
-    #[Groups(['category_basic'])]
+    #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'children')]
-    #[Groups(['category_parent'])]
-    private ?self $parent = null;
-
-    #[ORM\OneToMany(mappedBy: 'parent', targetEntity: self::class)]
-    #[Groups(['category_children'])]
-    #[MaxDepth(1)]
-    private Collection $children;
-
-    #[ORM\OneToMany(mappedBy: 'category', targetEntity: Product::class)]
-    #[Groups(['category_products'])]
-    private Collection $products;
-
-    #[ORM\ManyToMany(targetEntity: ItemFeature::class, inversedBy: 'categories')]
-    #[Groups(['category_features'])]
-    private Collection $features;
-
     #[ORM\Column]
-    #[Groups(['category_basic'])]
-    private ?bool $leaf = false;
+    #[ORM\OneToOne(targetEntity: "Category")]
+    #[ORM\JoinColumn(name: "parent_id", referencedColumnName: "id")]
+    private ?int $parent = null;
 
-    #[ORM\Column(nullable: false)]
-    #[Groups(['category_basic'])]
-    private ?bool $active = true;
-
-    public function __construct()
-    {
-        $this->children = new ArrayCollection();
-        $this->products = new ArrayCollection();
-        $this->features = new ArrayCollection();
-    }
 
     public function getId(): ?int
     {
@@ -71,122 +39,14 @@ class Category
         return $this;
     }
 
-    public function getParent(): ?self
+    public function getParent(): ?string
     {
         return $this->parent;
     }
 
-    public function setParent(?self $parent): self
+    public function setParent(string $parent): self
     {
         $this->parent = $parent;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Product>
-     */
-    public function getProducts(): Collection
-    {
-        return $this->products;
-    }
-
-    public function addProduct(Product $product): self
-    {
-        if (!$this->products->contains($product)) {
-            $this->products->add($product);
-            $product->setCategory($this);
-        }
-
-        return $this;
-    }
-
-    public function removeProduct(Product $product): self
-    {
-        if ($this->products->removeElement($product)) {
-            // set the owning side to null (unless already changed)
-            if ($product->getCategory() === $this) {
-                $product->setCategory(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, ItemFeature>
-     */
-    public function getFeatures(): Collection
-    {
-        return $this->features;
-    }
-
-    public function addFeature(ItemFeature $feature): self
-    {
-        if (!$this->features->contains($feature)) {
-            $this->features->add($feature);
-        }
-
-        return $this;
-    }
-
-    public function removeFeature(ItemFeature $feature): self
-    {
-        $this->features->removeElement($feature);
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, self>
-     */
-    public function getChildren(): Collection
-    {
-        return $this->children;
-    }
-
-    public function addChild(self $child): self
-    {
-        if (!$this->children->contains($child)) {
-            $this->children->add($child);
-            $child->setParent($this);
-        }
-
-        return $this;
-    }
-
-    public function removeChild(self $child): self
-    {
-        if ($this->children->removeElement($child)) {
-            // set the owning side to null (unless already changed)
-            if ($child->getParent() === $this) {
-                $child->setParent(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function isLeaf(): ?bool
-    {
-        return $this->leaf;
-    }
-
-    public function setLeaf(bool $leaf): self
-    {
-        $this->leaf = $leaf;
-
-        return $this;
-    }
-
-    public function isActive(): ?bool
-    {
-        return $this->active;
-    }
-
-    public function setActive(bool $active): self
-    {
-        $this->active = $active;
 
         return $this;
     }
--- src/Repository/CategoryBrandRepository.php
+++ src/Repository/CategoryBrandRepository.php
@@ -2,26 +2,26 @@
 
 namespace App\Repository;
 
-use App\Entity\Category;
+use App\Entity\CategoryBrand;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<Category>
+ * @extends ServiceEntityRepository<CategoryBrand>
  *
- * @method Category|null find($id, $lockMode = null, $lockVersion = null)
- * @method Category|null findOneBy(array $criteria, array $orderBy = null)
- * @method Category[]    findAll()
- * @method Category[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method CategoryBrand|null find($id, $lockMode = null, $lockVersion = null)
+ * @method CategoryBrand|null findOneBy(array $criteria, array $orderBy = null)
+ * @method CategoryBrand[]    findAll()
+ * @method CategoryBrand[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class CategoryRepository extends ServiceEntityRepository
+class CategoryBrandRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Category::class);
+        parent::__construct($registry, CategoryBrand::class);
     }
 
-    public function add(Category $entity, bool $flush = false): void
+    public function add(CategoryBrand $entity, bool $flush = false): void
     {
         $this->getEntityManager()->persist($entity);
 
@@ -30,7 +30,7 @@ class CategoryRepository extends ServiceEntityRepository
         }
     }
 
-    public function remove(Category $entity, bool $flush = false): void
+    public function remove(CategoryBrand $entity, bool $flush = false): void
     {
         $this->getEntityManager()->remove($entity);
 
@@ -40,7 +40,7 @@ class CategoryRepository extends ServiceEntityRepository
     }
 
 //    /**
-//     * @return Category[] Returns an array of Category objects
+//     * @return CategoryBrand[] Returns an array of CategoryBrand objects
 //     */
 //    public function findByExampleField($value): array
 //    {
@@ -54,7 +54,7 @@ class CategoryRepository extends ServiceEntityRepository
 //        ;
 //    }
 
-//    public function findOneBySomeField($value): ?Category
+//    public function findOneBySomeField($value): ?CategoryBrand
 //    {
 //        return $this->createQueryBuilder('c')
 //            ->andWhere('c.exampleField = :val')
