0bfd6327cc68cd174434ae3af2e8a2ae74d23acc
shahryar.jahanshahloo <shahryar.jahanshahloo@gmail.com>
2022-08-22 16:40:23 +0430

    added relations to Product

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ProductRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
@@ -19,6 +21,21 @@ class Product
     #[ORM\ManyToOne(inversedBy: 'products')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Category $category = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\OneToMany(mappedBy: 'product', targetEntity: Variant::class)]
+    private Collection $variants;
+
+    #[ORM\ManyToMany(targetEntity: Seller::class, inversedBy: 'products')]
+    private Collection $sellers;
+
+    public function __construct()
+    {
+        $this->variants = new ArrayCollection();
+        $this->sellers = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -45,6 +62,72 @@ class Product
     public function setCategory(?Category $category): self
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Variant>
+     */
+    public function getVariants(): Collection
+    {
+        return $this->variants;
+    }
+
+    public function addVariant(Variant $variant): self
+    {
+        if (!$this->variants->contains($variant)) {
+            $this->variants->add($variant);
+            $variant->setProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVariant(Variant $variant): self
+    {
+        if ($this->variants->removeElement($variant)) {
+            // set the owning side to null (unless already changed)
+            if ($variant->getProduct() === $this) {
+                $variant->setProduct(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Seller>
+     */
+    public function getSellers(): Collection
+    {
+        return $this->sellers;
+    }
+
+    public function addSeller(Seller $seller): self
+    {
+        if (!$this->sellers->contains($seller)) {
+            $this->sellers->add($seller);
+        }
+
+        return $this;
+    }
+
+    public function removeSeller(Seller $seller): self
+    {
+        $this->sellers->removeElement($seller);
 
         return $this;
     }
