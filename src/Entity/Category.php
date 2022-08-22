598c14225913f26f4a6f0bda2f7f277d4041e32e
shahryar.jahanshahloo <shahryar.jahanshahloo@gmail.com>
2022-08-22 09:55:17 +0430

    updated relations

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -5,7 +5,8 @@ namespace App\Entity;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
-use App\Entity\Brand;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 class Category
@@ -15,24 +16,34 @@ class Category
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(type: "string", length: 255, unique: true, nullable: false)]
-    private $name = null;
+    #[ORM\Column(length: 255, unique: true, nullable: false)]
+    private ?string $name = null;
 
-    #[ORM\OneToOne(targetEntity: "Category")]
-    #[ORM\JoinColumn(name: "parent_id", referencedColumnName: "id")]
-    private $parent;
+    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'children')]
+    private ?self $parent = null;
 
-    #[ORM\ManyToMany(targetEntity: "Brand", mappedBy: "categories")]
-    private $brands;
+    #[ORM\OneToMany(mappedBy: 'parent', targetEntity: self::class)]
+    private Collection $children;
 
-    #[ORM\OneToMany(mappedBy: "category", targetEntity: "Product")]
-    private $products;
+    #[ORM\ManyToMany(targetEntity: Brand::class, mappedBy: "categories")]
+    private Collection $brands;
 
-    #[ORM\ManyToMany(targetEntity: "Feature", inversedBy: "categories")]
-    #[JoinTable(name: "category_features")]
-    #[JoinColumn(name: "category_id", referencedColumnName: "id")]
-    #[InverseJoinColumn(name: "feature_id", referencedColumnName: "id")]
-    private $features;
+    #[ORM\OneToMany(mappedBy: "category", targetEntity: Product::class)]
+    private Collection $products;
+
+    #[ORM\ManyToMany(targetEntity: ItemFeature::class, inversedBy: "categories")]
+    #[ORM\JoinTable(name: "category_features")]
+    #[ORM\JoinColumn(name: "category_id", referencedColumnName: "id")]
+    #[ORM\InverseJoinColumn(name: "feature_id", referencedColumnName: "id")]
+    private Collection $features;
+
+    public function __construct()
+    {
+        $this->brands = new ArrayCollection();
+        $this->products = new ArrayCollection();
+        $this->features = new ArrayCollection();
+        $this->children = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -51,14 +62,44 @@ class Category
         return $this;
     }
 
-    public function getParent(): ?string
+    public function getParent(): ?self
     {
         return $this->parent;
     }
 
-    public function setParent(string $parent): self
+    public function setParent(?self $parent): self
     {
         $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, self>
+     */
+    public function getChildren(): Collection
+    {
+        return $this->children;
+    }
+
+    public function addChild(self $child): self
+    {
+        if (!$this->children->contains($child)) {
+            $this->children->add($child);
+            $child->setParent($this);
+        }
+
+        return $this;
+    }
+
+    public function removeChild(self $child): self
+    {
+        if ($this->children->removeElement($child)) {
+            // set the owning side to null (unless already changed)
+            if ($child->getParent() === $this) {
+                $child->setParent(null);
+            }
+        }
 
         return $this;
     }
