11533b41f4766f4caa80722ab827d22cf6e859cb
shahryar.jahanshahloo <shahryar.jahanshahloo@gmail.com>
2022-08-24 01:20:12 +0430

    added dtos

--- src/Controller/BrandController.php
+++ src/Controller/BrandController.php
@@ -2,95 +2,91 @@
 
 namespace App\Controller;
 
+use App\DTO\Transformer\BrandTransformer;
 use App\Entity\Category;
-use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Brand;
-use App\Service\BrandManager;
-use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/brand', name: 'app_brand_')]
 class BrandController extends AbstractController
 {
-    protected BrandManager $brandManager;
-
-    public function __construct(BrandManager $brandManager)
-    {
-        $this->brandManager = $brandManager;
-    }
-
     //TODO: admin auth
     #[Route('/create', name: 'create', methods: ['POST'])]
     public function create(ManagerRegistry $doctrine, Request $req): Response
     {
-        try {
-            $requestBody = $this->brandManager->getRequestBody($req);
-            $validatedBody = $this->brandManager->validateBrandArray($requestBody);
-            if (array_key_exists('error', $validatedBody)) return $this->json(['message' => $validatedBody['error']], 400);
-            $brand = $this->brandManager->createEntityFromArray($validatedBody);
-            $doctrine->getRepository(Brand::class)->add($brand, true);
-            return $this->json(["brand" => $brand]);
-        } catch (Exception $exception) {
-            return $this->json(['message' => $exception->getMessage()], 500);
-        }
+        $name = trim($req->get("name"));
+        $description = $req->get("description");
+        if (!$name) return new Response("invalid name", 400);
+        if ($description == null) $description = "";
+        $brandRepo = $doctrine->getRepository(Brand::class);
+        if ($brandRepo->findOneByName($name)) return new Response('name already exists', 400);
+        $brand = new Brand();
+        $brand->setName($name);
+        $brand->setDescription($description);
+        $entityManager = $doctrine->getManager();
+        $entityManager->persist($brand);
+        $entityManager->flush();
+        return $this->json([
+            "brand" => BrandTransformer::transformFromEntity($brand)
+        ]);
+    }
+
+    //TODO: admin auth
+    #[Route('/add/category', name: 'add_category', methods: ['PATCH'])]
+    public function addCategory(ManagerRegistry $doctrine, Request $req): Response
+    {
+        $brandName = $req->get("brandName");
+        $categoryName = $req->get("categoryName");
+        $brand = new Brand();
+        $brand->setName($brandName);
+//        $brand = $doctrine->getRepository(Brand::class)->findOneByName($brandName);
+        $category = $doctrine->getRepository(Category::class)->findOneByName($categoryName);
+        $brand->addCategory($category);
+        $entityManager = $doctrine->getManager();
+        $entityManager->persist($brand);
+        $entityManager->flush();
+        return $this->json([
+            "categories" => $brand->getCategories()
+        ]);
     }
 
     //TODO: admin auth
     #[Route('/delete', name: 'delete', methods: ['DELETE'])]
-    public function delete(Request $req): Response
+    public function delete(ManagerRegistry $doctrine, Request $req): Response
     {
-        try {
-            $name = $req->get('name');
-            $message = $this->brandManager->removeUnusedByName($name);
-            if (array_key_exists('error', $message)) return $this->json(['message' => $message['error']], 400);
-            return $this->json(['message' => $message['message']]);
-        } catch (Exception $exception) {
-            return $this->json(['message' => $exception->getMessage()], 500);
-        }
+        return $this->json([]);
+    }
+
+    //TODO: admin auth
+    #[Route('/remove/category', name: 'remove_category', methods: ['DELETE'])]
+    public function removeCategory(ManagerRegistry $doctrine, Request $req): Response
+    {
+        return $this->json([]);
     }
 
     //TODO: admin auth
     #[Route('/update', name: 'update', methods: ['PATCH'])]
-    public function update(Request $req): Response
+    public function update(ManagerRegistry $doctrine, Request $req): Response
     {
-        try {
-            [$name, $updates] = $this->brandManager->getRequestBody($req);
-            $message = $this->brandManager->update($name, $updates);
-            if (array_key_exists('error', $message)) return $this->json(['message' => $message['error']], 400);
-            return $this->json(['message' => $message['message']]);
-        } catch (Exception $exception) {
-            return $this->json(['message' => $exception->getMessage()], 500);
-        }
+        return $this->json([]);
     }
 
-    #[Route('/{name}', name: 'brand_details', methods: ['GET'])]
-    public function getBrand(ManagerRegistry $doctrine, string $name, SerializerInterface $serializer): Response
+    #[Route('/{name}/description', name: 'description', methods: ['GET'])]
+    public function description(ManagerRegistry $doctrine, Request $req, string $name): Response
     {
-        try {
-            $brand = $doctrine->getRepository(Brand::class)->findOneByName($name);
-            if (!$brand) return $this->json(['message' => 'brand not found'], 400);
-            $json = $serializer->serialize($brand, 'json', ['groups' => ['brand_basic']]);
-            return $this->json(['brand' => $json]);
-        } catch (Exception $exception) {
-            return $this->json(['message' => $exception->getMessage()], 500);
-        }
+        $brand = $doctrine->getRepository(Brand::class)->findOneByName($name);
+        return $this->json([
+            "description" => $brand->getDescription()
+        ]);
     }
 
     #[Route('/search', name: 'search', methods: ['GET'])]
     public function search(ManagerRegistry $doctrine, Request $req): Response
     {
-        try {
-            $q = $req->query->get('query');
-            $brands = $doctrine->getRepository(Brand::class)->findManyByQuery($q);
-            return $this->json(['brands' => $brands]);
-        } catch (Exception $exception) {
-            return $this->json(['message' => $exception->getMessage()], 500);
-        }
+        return $this->json([]);
     }
-
-    //TODO show brand products with filters
 }
