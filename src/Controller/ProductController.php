1d42b75182d97675e4a16e8ea6bbad8800b76b1d
shahryar.jahanshahloo <shahryar.jahanshahloo@gmail.com>
2022-08-24 16:32:15 +0430

    added product routes

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,16 +3,50 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Product;
+use Symfony\Component\Serializer\SerializerInterface;
 
+#[Route('/product', name: 'app_product_')]
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    #[Route('/create', name: 'create', methods: ['POST'])]
+    public function create(ManagerRegistry $doctrine, Request $req): Response
     {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
-        ]);
+        return $this->json([]);
+    }
+
+    #[Route('/update', name: 'update', methods: ['PATCH'])]
+    public function update(ManagerRegistry $doctrine, Request $req): Response
+    {
+        return $this->json([]);
+    }
+
+    #[Route('/delete', name: 'delete', methods: ['DELETE'])]
+    public function delete(ManagerRegistry $doctrine, Request $req): Response
+    {
+        return $this->json([]);
+    }
+
+    //needs filters
+    #[Route('/products', name: 'products', methods: ['GET'])]
+    public function showProducts(ManagerRegistry $doctrine, Request $req): Response
+    {
+        return $this->json([]);
+    }
+
+    #[Route('/{name}', name: 'showOne', methods: ['GET'])]
+    public function showOneProduct(ManagerRegistry $doctrine, Request $req): Response
+    {
+        return $this->json([]);
+    }
+
+    #[Route('/search', name: 'search', methods: ['GET'])]
+    public function search(ManagerRegistry $doctrine, Request $req): Response
+    {
+        return $this->json([]);
     }
 }
