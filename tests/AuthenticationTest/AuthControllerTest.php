10016c21c6a68ebee78836839ad3d302542ad4dd
do0ry <dorieeeh.ai@yahoo.com>
2022-08-26 13:41:56 +0430

    uncomment some lines of code

--- tests/AuthenticationTest/AuthControllerTest.php
+++ tests/AuthenticationTest/AuthControllerTest.php
@@ -13,35 +13,35 @@ class AuthControllerTest extends JsonApiTestCase
 
     protected array $defaultUser = ['username'=>'09128464485' ,'password'=>'123456789'];
 
-//    public function testUserRegister()
-//    {
-//
-//        $body = [
-//            'name'=>'jery',
-//            'email'=>'cacvoaasfdsaqsgweo@gmail.com',
-//            'lastName'=>'coca',
-//            'phoneNumber'=>'09182836949',
-//            'password'=>'123456789',
-//            'roles'=>['ROLE_SELLER']
-//        ];
-//
-//        $this->client->request(
-//            'POST',
-//            'http://localhost:70/api/user/register',
-//            [],
-//            [],
-//            [],
-//            json_encode($body)
-//        );
-//
-//        $response = $this->client->getResponse()->getContent();
-//
-//        $data = json_decode($response,true);
-//
-//        self::assertArrayHasKey('token',$data);
-//
-//        self::assertArrayHasKey('refresh_token',$data);
-//    }
+    public function testUserRegister()
+    {
+
+        $body = [
+            'name'=>'jery',
+            'email'=>'cacvoaasfdsaqsgweo@gmail.com',
+            'lastName'=>'coca',
+            'phoneNumber'=>'09182836949',
+            'password'=>'123456789',
+            'roles'=>['ROLE_SELLER']
+        ];
+
+        $this->client->request(
+            'POST',
+            'http://localhost:70/api/user/register',
+            [],
+            [],
+            [],
+            json_encode($body)
+        );
+
+        $response = $this->client->getResponse()->getContent();
+
+        $data = json_decode($response,true);
+
+        self::assertArrayHasKey('token',$data);
+
+        self::assertArrayHasKey('refresh_token',$data);
+    }
 
     public function testUserLogin()
     {
