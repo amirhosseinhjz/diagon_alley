a0fe57b08da811dc3b1cc7d0aebe3a37ac3013eb
shahryar.jahanshahloo <shahryar.jahanshahloo@gmail.com>
2022-08-29 11:55:08 +0430

    fixed error handling on controllers

--- src/Entity/Brand.php
+++ src/Entity/Brand.php
@@ -14,26 +14,23 @@ class Brand
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['brand_basic'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255, unique: true, nullable: false)]
+    #[Groups(['brand_basic'])]
     private ?string $name = null;
 
     #[ORM\Column(length: 511, nullable: true)]
+    #[Groups(['brand_basic'])]
     private ?string $description = null;
 
-    #[ORM\ManyToMany(targetEntity: Category::class, mappedBy: 'brands')]
-    #[Groups(['show_categories'])]
-    //TODO remove relation with categories
-    private Collection $categories;
-
     #[ORM\OneToMany(mappedBy: 'brand', targetEntity: Product::class)]
-    #[Groups(['show_products'])]
+    #[Groups(['brand_products'])]
     private Collection $products;
 
     public function __construct()
     {
-        $this->categories = new ArrayCollection();
         $this->products = new ArrayCollection();
     }
 
@@ -62,33 +59,6 @@ class Brand
     public function setDescription(?string $description): self
     {
         $this->description = $description;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Category>
-     */
-    public function getCategories(): Collection
-    {
-        return $this->categories;
-    }
-
-    public function addCategory(Category $category): self
-    {
-        if (!$this->categories->contains($category)) {
-            $this->categories->add($category);
-            $category->addBrand($this);
-        }
-
-        return $this;
-    }
-
-    public function removeCategory(Category $category): self
-    {
-        if ($this->categories->removeElement($category)) {
-            $category->removeBrand($this);
-        }
 
         return $this;
     }
