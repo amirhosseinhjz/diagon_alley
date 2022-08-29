9b6edefe7d4aff99525550b08e4363075cf8c739
shahryar.jahanshahloo <shahryar.jahanshahloo@gmail.com>
2022-08-29 15:11:42 +0430

    updated product controller

--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use App\Service\BrandManager;
 
 class BrandTest extends KernelTestCase
 {
--- tests/CategoryTest.php
+++ tests/CategoryTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use App\Service\CategoryManager;
 
 class CategoryTest extends KernelTestCase
 {
