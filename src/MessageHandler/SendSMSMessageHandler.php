6ef62d3b7728862cb8f1232c933f55d6c02cd6b9
amirhosseinhjz <amirhosseinhjz@outlook.com>
2022-09-04 12:22:59 +0430

    mellipayamak webservice added

--- src/MessageHandler/SendSMSMessageHandler.php
+++ src/MessageHandler/SendSMSMessageHandler.php
@@ -4,11 +4,26 @@ namespace App\MessageHandler;
 
 use App\Message\SendSMSMessage;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Melipayamak\MelipayamakApi;
+
 
 final class SendSMSMessageHandler implements MessageHandlerInterface
 {
     public function __invoke(SendSMSMessage $message)
     {
-        // do something with your message
+        $this->send($message);
+    }
+
+
+    private function send(SendSMSMessage $message)
+    {
+        $username = "env('SMS_SERVICE_USERNAME')";
+        $password = "env('SMS_SERVICE_PASSWORD')";
+        $api = new MelipayamakApi($username,$password);
+        $sms = $api->sms();
+        $to = $message->getNumber();
+        $from = "env('SMS_SERVICE_FROM')";
+        $text = $message->getMessage();
+        $sms->send($to,$from,$text);
     }
 }
