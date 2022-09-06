3ee90b61f37b4980cc022262319a7c5d5aaaeca1
do0ry <dorieeeh.ai@yahoo.com>
2022-09-07 03:44:03 +0430

    add trait and changes on SendEmailMessageHandler

--- src/MessageHandler/SendEmailMessageHandler.php
+++ src/MessageHandler/SendEmailMessageHandler.php
@@ -3,6 +3,7 @@
 namespace App\MessageHandler;
 
 use App\Message\SendEmailMessage;
+use App\Trait\Email\EmailPreparation;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
@@ -11,6 +12,7 @@ use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 #[AsMessageHandler]
 final class SendEmailMessageHandler implements MessageHandlerInterface
 {
+    use EmailPreparation;
     private $mailerService;
 
     public function __construct(MailerInterface $mailer)
@@ -22,65 +24,5 @@ final class SendEmailMessageHandler implements MessageHandlerInterface
     {
         $email = $this->emailPreparation(new TemplatedEmail(), $message);
         $this->mailerService->send($email);
-    }
-
-    private function emailPreparation(TemplatedEmail $email,SendEmailMessage $message)
-    {
-        $email = $email->from($message->getEmailFrom())->to($message->getEmailTo());
-
-        if ($message->getSubject())
-        {
-            $email = $email->subject($message->getSubject());
-        }
-
-        if ($message->getHtmlTemplatePath())
-        {
-            $email = $email->htmlTemplate($message->getHtmlTemplatePath());
-        }
-
-        if ($message->getTextTemplate())
-        {
-            $email = $email->textTemplate($message->getTextTemplate());
-        }
-
-        if ($message->getContext())
-        {
-            $email = $email->context($message->getContext());
-        }
-
-        if ($message->getAttachFromPath())
-        {
-            $email = $email->attachFromPath
-            (
-                $message->getAttachFromPath()['path'],
-                array_key_exists('name',$message->getAttachFromPath()) ?: null
-            );
-        }
-
-        if ($message->getEmbedFromPath())
-        {
-            $email = $email->embedFromPath
-            (
-                $message->getEmbedFromPath()['path'],
-                array_key_exists('name',$message->getEmbedFromPath()) ?: null
-            );
-        }
-
-        if ($message->getBcc())
-        {
-            $email = $email->bcc($message->getContext());
-        }
-
-        if ($message->getCc())
-        {
-            $email = $email->cc($message->getContext());
-        }
-
-        if ($message->getText())
-        {
-            $email = $email->text($message->getText());
-        }
-
-        return $email;
     }
 }
---
+++ src/Trait/Email/EmailPreparation.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Trait\Email;
+
+use App\Message\SendEmailMessage;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+
+trait EmailPreparation
+{
+    private function emailPreparation(TemplatedEmail $email,SendEmailMessage $message)
+    {
+        $email = $email->from($message->getEmailFrom())->to($message->getEmailTo());
+
+        if ($message->getSubject())
+        {
+            $email = $email->subject($message->getSubject());
+        }
+
+        if ($message->getHtmlTemplatePath())
+        {
+            $email = $email->htmlTemplate($message->getHtmlTemplatePath());
+        }
+
+        if ($message->getTextTemplate())
+        {
+            $email = $email->textTemplate($message->getTextTemplate());
+        }
+
+        if ($message->getContext())
+        {
+            $email = $email->context($message->getContext());
+        }
+
+        if ($message->getAttachFromPath())
+        {
+            $email = $email->attachFromPath
+            (
+                $message->getAttachFromPath()['path'],
+                array_key_exists('name',$message->getAttachFromPath()) ?: null
+            );
+        }
+
+        if ($message->getEmbedFromPath())
+        {
+            $email = $email->embedFromPath
+            (
+                $message->getEmbedFromPath()['path'],
+                array_key_exists('name',$message->getEmbedFromPath()) ?: null
+            );
+        }
+
+        if ($message->getBcc())
+        {
+            $email = $email->bcc($message->getContext());
+        }
+
+        if ($message->getCc())
+        {
+            $email = $email->cc($message->getContext());
+        }
+
+        if ($message->getText())
+        {
+            $email = $email->text($message->getText());
+        }
+
+        return $email;
+    }
+}
