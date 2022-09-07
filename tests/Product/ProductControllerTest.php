6f165a1d70cd5b3e86ebc6d4a6ebceadd2fb5dcb
shahryar.jahanshahloo <shahryar.jahanshahloo@gmail.com>
2022-09-07 18:25:28 +0430

    updated tests

--- tests/Product/ProductControllerTest.php
+++ tests/Product/ProductControllerTest.php
@@ -13,46 +13,54 @@ class ProductControllerTest extends BaseJsonApiTestCase
 
     public function testCreateProduct()
     {
-        
+        $body = [
+            'name' => 'myProduct',
+            //TODO: category brand description
+        ];
+        $this->client->request('POST', self::ROUTE, content: json_encode($body));
+        $response = $this->client->getResponse();
+        self::assertResponseIsSuccessful($response->getStatusCode());
+        $data = json_decode($response->getContent(), true);
+        self::assertEquals($body['name'], $data['name']);
     }
-    
+
     public function testUpdateProduct()
     {
-        
+        $body = [];
     }
-    
+
     public function testDeleteProduct()
     {
-        
+
     }
-    
+
     public function testAddFeature()
     {
-        
+        $body = [];
     }
-    
+
     public function testRemoveFeature()
     {
-        
+        $body = [];
     }
-    
+
     public function testToggleActivity()
     {
-        
+        $body = [];
     }
 
     public function testGetBrandProducts()
     {
-        
+        $body = [];
     }
-    
-     public function testGetCategoryProducts()
+
+    public function testGetCategoryProducts()
     {
-        
+        $body = [];
     }
 
     public function testGetOneProduct()
     {
-        
+
     }
 }
