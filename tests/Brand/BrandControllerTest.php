2868d2946fb0fa4e0dbef6a13067599cd7c63d23
shahryar.jahanshahloo <shahryar.jahanshahloo@gmail.com>
2022-09-07 15:29:54 +0430

    initiated test cases

---
+++ tests/Brand/BrandControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Tests\Brand;
+
+use App\Tests\Base\BaseJsonApiTestCase;
+
+/**
+ * @group brand
+ */
+class BrandControllerTest extends BaseJsonApiTestCase
+{
+    public function testCreateBrand()
+    {
+
+    }
+
+    public function testUpdateBrand()
+    {
+
+    }
+
+    public function testDeleteBrand()
+    {
+
+    }
+
+    public function testGetAllBrands()
+    {
+
+    }
+
+    public function testGetOneBrand()
+    {
+
+    }
+
+    public function testSearchBrand()
+    {
+
+    }
+}
---
+++ tests/Category/CategoryControllerTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Tests\Category;
+
+use App\Tests\Base\BaseJsonApiTestCase;
+
+/**
+ * @group category
+ */
+class CategoryControllerTest extends BaseJsonApiTestCase
+{
+    public function testCreateCategory()
+    {
+
+    }
+
+    public function testUpdateCategory()
+    {
+
+    }
+
+    public function testDeleteCategory()
+    {
+
+    }
+
+    public function testGetMainCategories()
+    {
+
+    }
+
+    public function testGetOneCategory()
+    {
+
+    }
+
+    public function testToggleActivity()
+    {
+
+    }
+
+    public function testAddFeature()
+    {
+
+    }
+
+    public function testRemoveFeature()
+    {
+
+    }
+
+    public function testGetCategoryParents()
+    {
+
+    }
+
+    public function testGetCategoryBrands()
+    {
+
+    }
+
+    public function testGetCategoryFeatures()
+    {
+
+    }
+}
