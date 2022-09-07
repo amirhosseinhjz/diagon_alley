c1ecb86761861e06780e6ff31e2e70d8a83d8924
shahryar.jahanshahloo <shahryar.jahanshahloo@gmail.com>
2022-09-07 15:29:54 +0430

    initiated test cases

---
+++ tests/Product/ProductControllerTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Tests\Product;
+
+use App\Tests\Base\BaseJsonApiTestCase;
+
+/**
+ * @group product
+ */
+class ProductControllerTest extends BaseJsonApiTestCase
+{
+    public function testCreateProduct()
+    {
+        
+    }
+    
+    public function testUpdateProduct()
+    {
+        
+    }
+    
+    public function testDeleteProduct()
+    {
+        
+    }
+    
+    public function testAddFeature()
+    {
+        
+    }
+    
+    public function testRemoveFeature()
+    {
+        
+    }
+    
+    public function testToggleActivity()
+    {
+        
+    }
+
+    public function testGetBrandProducts()
+    {
+        
+    }
+    
+     public function testGetCategoryProducts()
+    {
+        
+    }
+
+    public function testGetOneProduct()
+    {
+        
+    }
+}
