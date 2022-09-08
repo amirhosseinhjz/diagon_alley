14959256ae16f467b0c340b7f774b1f4316d8c90
do0ry <dorieeeh.ai@yahoo.com>
2022-09-09 03:55:50 +0430

    add needed query methods

--- src/Repository/ShipmentRepository/ShipmentRepository.php
+++ src/Repository/ShipmentRepository/ShipmentRepository.php
@@ -39,28 +39,25 @@ class ShipmentRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Shipment[] Returns an array of Shipment objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('s.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findWithItems($id)
+    {
+        return $this->createQueryBuilder('s')
+            ->innerJoin('s.shipmentItems','si')
+            ->addSelect('si')
+            ->andWhere('s.id = :id')
+            ->setParameter('id',$id)
+            ->getQuery()
+            ->getResult();
+    }
 
-//    public function findOneBySomeField($value): ?Shipment
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function findWithSeller($id)
+    {
+        return $this->createQueryBuilder('sh')
+            ->innerJoin('sh.seller','s')
+            ->addSelect('s')
+            ->andWhere('s.id = :id')
+            ->setParameter('id',$id)
+            ->getQuery()
+            ->getResult();
+    }
 }
