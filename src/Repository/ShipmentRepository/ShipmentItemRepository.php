9821ad9663fb7f32d3062e755cac1d20eb15103f
neda aalami <71936216+do0ry@users.noreply.github.com>
2022-09-09 04:18:28 +0430

    Update ShipmentItemRepository.php

--- src/Repository/ShipmentRepository/ShipmentItemRepository.php
+++ src/Repository/ShipmentRepository/ShipmentItemRepository.php
@@ -39,28 +39,4 @@ class ShipmentItemRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return ShipmentItem[] Returns an array of ShipmentItem objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('s.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?ShipmentItem
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
