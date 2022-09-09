0f3bb6e07eaef81c04920f59fa3c61c834d481b4
shahryar.jahanshahloo <shahryar.jahanshahloo@gmail.com>
2022-09-09 21:36:14 +0430

    added brand tests

--- src/DataFixtures/BrandFixtures.php
+++ src/DataFixtures/BrandFixtures.php
@@ -9,6 +9,10 @@ use Doctrine\Persistence\ObjectManager;
 
 class BrandFixtures extends Fixture implements FixtureGroupInterface
 {
+    public const UPDATE_NAME = 'ToBeUpdated';
+    public const DELETE_NAME = 'ToBeDeleted';
+    public const READ_NAME = 'brandName';
+
     public static function getGroups(): array
     {
         return ['brand'];
@@ -16,27 +20,42 @@ class BrandFixtures extends Fixture implements FixtureGroupInterface
 
     public function load(ObjectManager $manager): void
     {
-        $this->loadBrands($manager);
+        $this->loadBrandsForRead($manager);
+        $this->loadBrandForUpdate($manager);
+        $this->loadBrandForDelete($manager);
     }
 
-    public function loadBrands(ObjectManager $manager)
+    public function loadBrandsForRead(ObjectManager $manager)
     {
+        $brand = new Brand();
+        $brand->setName(self::READ_NAME);
+        $manager->persist($brand);
+
         $brand1 = new Brand();
         $brand1->setName("brand1");
+        $manager->persist($brand1);
 
         $brand2 = new Brand();
         $brand2->setName("brand2");
+        $manager->persist($brand2);
 
-        $manager->persist($brand1);
         $manager->flush();
     }
 
     public function loadBrandForUpdate(ObjectManager $manager)
     {
-        $brandToUpdate = new Brand();
-        $brandToUpdate->setName('brandToUpdate');
+        $brand = new Brand();
+        $brand->setName(self::UPDATE_NAME);
+        $brand->setDescription('initial description');
+        $manager->persist($brand);
+        $manager->flush();
+    }
 
-        $manager->persist($brandToUpdate);
+    public function loadBrandForDelete(ObjectManager $manager)
+    {
+        $brand = new Brand();
+        $brand->setName(self::DELETE_NAME);
+        $manager->persist($brand);
         $manager->flush();
     }
 }
