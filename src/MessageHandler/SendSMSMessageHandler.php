66ada00e1389568dba135453fcbfe70c62cfa04b
amirhosseinhjz <amirhosseinhjz@outlook.com>
2022-09-13 12:15:18 +0430

    otp finished

--- src/MessageHandler/SendSMSMessageHandler.php
+++ src/MessageHandler/SendSMSMessageHandler.php
@@ -5,66 +5,37 @@ namespace App\MessageHandler;
 use App\Message\SendSMSMessage;
 use GuzzleHttp\Client;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
-use Melipayamak\MelipayamakApi;
 
 
 final class SendSMSMessageHandler implements MessageHandlerInterface
 {
+
+    public function __construct()
+    {
+        $this->username = $_ENV['MELLI_USERNAME'];
+        $this->password = $_ENV['MELLI_PASSWORD'];
+        $this->from = $_ENV['MELLI_FROM_NUMBER'];
+        $this->url = $_ENV['MELLI_URL'];
+        $this->client = new Client();
+    }
+
     public function __invoke(SendSMSMessage $message)
     {
-        try{
-        $this->send3($message);
-        }catch(\Exception $e){
-            dump($e->getMessage());
-        }
+        $this->send($message);
     }
 
 
-    private function send(SendSMSMessage $message)
-    {
-        $username = $_ENV['MELLI_USERNAME'];
-        $password = $_ENV['MELLI_PASSWORD'];
-        $api = new MelipayamakApi($username,$password);
-        $sms = $api->sms('soap');
-        $to = [$message->getNumber()];
-        $from = $_ENV['MELLI_FROM_NUMBER'];
-        $text = $message->getMessage();
-        $isFlash = true;
-        dump('sending sms to '.json_encode($to));
-        dump($sms->send(json_encode($to),$from,$text,$isFlash));
-    }
 
-    public function send2(SendSMSMessage $message)
-    {
-        try{
-            $username = $_ENV['MELLI_USERNAME'];
-            $password = $_ENV['MELLI_PASSWORD'];
-            $api = new MelipayamakApi($username,$password);
-            $sms = $api->sms();
-            $to = [$message->getNumber()];
-            $from = $_ENV['MELLI_FROM_NUMBER'];
-            $text = $message->getMessage();
-            $response = $sms->send($to,$from,$text);
-            $json = json_decode($response);
-            dump($json->Value);
-        }catch(Exception $e){
-            echo $e->getMessage();
-        }
-    }
 
-    public function send3(SendSMSMessage $message)
+    public function send(SendSMSMessage $message)
     {
-        $url = 'https://api.payamak-panel.com/post/Send.asmx/SendSimpleSMS2?username=&password=&to=&from=&text=&isflash=';
-        $url = str_replace('username=', 'username='.$_ENV['MELLI_USERNAME'], $url);
-        $url = str_replace('password=', 'password='.$_ENV['MELLI_PASSWORD'], $url);
+        $url = $this->url;
+        $url = str_replace('username=', 'username='.$this->username, $url);
+        $url = str_replace('password=', 'password='.$this->password, $url);
+        $url = str_replace('from=', 'from='.$this->from, $url);
         $url = str_replace('to=', 'to='.$message->getNumber(), $url);
-        $url = str_replace('from=', 'from='.$_ENV['MELLI_FROM_NUMBER'], $url);
         $url = str_replace('text=', 'text='.$message->getMessage(), $url);
         $url = str_replace('isflash=', 'isflash=true', $url);
-        dump($url);
-        $client = new Client();
-        $response = $client->get($url);
-//        $response = http_get($url);
-        dump($response);
+        $this->client->get($url);
     }
 }
